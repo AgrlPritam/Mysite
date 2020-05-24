674e395b1c4e14d0d43b73b1ab1f4357b34d7afc
Pritam Pratik Agrawal <pritamagrawal95@gmail.com>
2020-05-24 12:06:31 +0530

    Completing UI and urls.py correction

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
 from django.contrib.auth import views
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
-    url(r'',include('blogs.urls')),
-    url(r'accounts/login/$',views.login,name='login'),
-    url(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'}),
+    url(r'',include('blog.urls')),
+    url(r'^accounts/login/$',LoginView.as_view(),name='login'),
+    url(r'^accounts/logout/$',LogoutView.as_view(),name='logout',kwargs={'next_page':'/'}),
     path('admin/', admin.site.urls),
 ]
